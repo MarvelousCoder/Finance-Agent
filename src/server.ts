37f8daf75b7aef33e1f4d6ea27088164025f5c67
MarvelousCoder <mehulkatwe10@gmail.com>
2026-06-06 17:59:27 +0530

    Simple UI Interface

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import agentRouter from "./routes/agent";
 import traceRouter from "./routes/trace";
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 /**
  * Routes
